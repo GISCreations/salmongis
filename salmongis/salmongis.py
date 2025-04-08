a2388c8de7c84e2857bda0dbc99b8ef04cdc5b55
Owen Bales <owesome140@gmail.com>
2025-04-08 10:21:46 -0400

    updated salmongis image

--- salmongis/salmongis.py
+++ salmongis/salmongis.py
@@ -86,3 +86,48 @@ class Map(ipyleaflet.Map):
 
         # Add the GeoJSON layer to the map
         self.add_layer(ipyleaflet.GeoJSON(data=geojson, **kwargs))
+    
+    def add_raster(self, filepath, **kwargs):
+        from localtileserver import TileClient, get_leaflet_tile_layer 
+        client = TileClient(filepath)
+        tile_layer = get_leaflet_tile_layer(client, **kwargs)
+        self = Map(zoom=3)
+        self.add(tile_layer)
+        self.center = client.center()
+        self.zoom = client.default_zoom
+    
+    def add_image (self, image, bounds=None, **kwargs):
+        """Adds an image to the map.
+        Args: image (str): The file path to the image.
+            
+        bounds (list, optional): The bounds for the image. Defaults to None.
+        **kwargs: Additional keyword arguments for the ipyleaflet. ImageOverlay layer.
+        """
+        if bounds is None: 
+            bounds = [[-90, -180] [90,1801]]
+        overlay = ipyleaflet.ImageOverlay(url=image, bounds=bounds, **kwargs)
+        self.add(overlay)
+
+    def add_video(self, video, bounds = None, **kwargs):
+        """Adds a video to the map.
+        Args: video (str): The file path to the video.
+            
+        bounds (list, optional): The bounds for the video. Defaults to None.
+        **kwargs: Additional keyword arguments for the ipyleaflet. VideoOverlay layer.
+        """
+        if bounds is None: 
+            bounds = [[-90, -180] [90, 180]]
+        overlay = ipyleaflet.VideoOverlay(url=video, bounds=bounds, **kwargs)
+        self.add(overlay)
+
+    def add_wms_layer(self, url, layers, transparent = True, **kwargs):
+        """
+        Adds a WMS layer to the map.
+
+        Args:
+            url (str): The WMS service URL.
+            layers (str): The layers to be displayed.
+            **kwargs: Additional keyword arguments for ipyleaflet.WMSLayer.
+        """
+        wms_layer = ipyleaflet.WMSLayer(url=url, layers=layers, format = format, transparent = transparent,  **kwargs)
+        self.add(wms_layer)
